e6dabf1a1efb82b072b4ea8a1b9116462e021cc4
Fabian Binz <fabian.binz@gmail.com>
2024-12-04 21:51:42 +0100

    style: restructure for code clarity

--- django_magic_context/__init__.py
+++ django_magic_context/__init__.py
@@ -1,36 +1,37 @@
 import inspect
-from typing import Any
+from typing import Any, Callable
 
 NOT_EVALUATED = object()
+
+
+def evaluate(d: dict[str, Any], key: str, value: Callable, args: list[str]):
+    evaluated = value(**{name: get(d, name, lazy=False) for name in args})
+    d[key] = evaluated
+    return evaluated
 
 
 def get(d: dict[str, Any], key: str, lazy=True):
     value = d[key]
 
-    if callable(value):
-        args = inspect.getargs(value.__code__)
-
-        if lazy:
-            evaluated = NOT_EVALUATED
-
-            def evaluate():
-                nonlocal evaluated
-
-                if evaluated is NOT_EVALUATED:
-                    evaluated = value(
-                        **{name: get(d, name, lazy=False) for name in args.args}
-                    )
-                    d[key] = evaluated
-                return evaluated
-
-            return evaluate
-
-        else:
-            evaluated = value(**{name: get(d, name, lazy=False) for name in args.args})
-            d[key] = evaluated
-            return evaluated
-    else:
+    # handle plain values
+    if not callable(value):
         return value
+
+    # handle non-lazy calls
+    args = inspect.getargs(value.__code__).args
+    if not lazy:
+        return evaluate(d, key, value, args)
+
+    # handle lazy calls
+    evaluated = NOT_EVALUATED
+
+    def lazy_evaluate():
+        nonlocal evaluated
+        if evaluated is NOT_EVALUATED:
+            evaluated = evaluate(d, key, value, args)
+        return evaluated
+
+    return lazy_evaluate
 
 
 def resolve(**kwargs):
