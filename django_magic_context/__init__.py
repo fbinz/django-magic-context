4454d80ae5da624dddd2c0b8d96f2d111de13a1f
Fabian Binz <fabian.binz@gmail.com>
2025-01-04 13:46:21 +0100

    feat: add helper function

--- django_magic_context/__init__.py
+++ django_magic_context/__init__.py
@@ -37,3 +37,33 @@ def get(context: dict[str, Any], key: str, lazy=True):
 
 def resolve(**kwargs):
     return {k: get(kwargs, k) if callable(v) else v for k, v in kwargs.items()}
+
+
+NO_DEFAULT = object()
+
+
+def cget(context, *keys, default=NO_DEFAULT):
+    """Helper function to get one or multiple keys from a context.
+
+    If one key is requested, one a single value is returned.
+    If multiple keys are requested, a list of values is returned.
+
+    If no default is given and the key does not exist, a KeyError is raised.
+
+    If the requested value is callable, it will be called.
+    """
+    result = []
+    for key in keys:
+        if default is NO_DEFAULT:
+            value = context[key]
+        else:
+            value = context.get(key, default)
+        if callable(value):
+            result.append(value())
+        else:
+            result.append(value)
+
+    if len(result) == 1:
+        return result[0]
+
+    return result
